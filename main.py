102fb907fe75ea98b7ec39e74cae4d7ba34179db
EvanPrograms <133557127+EvanPrograms@users.noreply.github.com>
2023-05-17 23:21:51 -0600

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ def is_iss_overhead():
     iss_latitude = float(data["iss_position"]["latitude"])
     iss_longitude = float(data["iss_position"]["longitude"])
 
-    #Your position is within +5 or -5 degrees of the ISS position.
+
 
     if MY_lat-5 <= iss_latitude <= MY_LAT+5 and MY_LONG-5 <= iss_longitude < MY_LONG+5:
         return True
@@ -50,10 +50,6 @@ while True:
                 msg="Subject:Look Up\n\nThe ISS is above you in the sky"
             )
 
-#If the ISS is close to my current position
-# and it is currently dark
-# Then send me an email to tell me to look up.
-# BONUS: run the code every 60 seconds.
 
 
 
